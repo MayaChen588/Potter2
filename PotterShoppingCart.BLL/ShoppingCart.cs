4f9d6b62294cb4c2108eab205476cc3ebaaf8ce1
mayachen <maya.chen@tpinformation.com.tw>
2017-09-27 15:37:50 +0800

    First Commit.

---
+++ PotterShoppingCart.BLL/ShoppingCart.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.BLL
+{
+    public class ShoppingCart
+    {
+        private decimal _price = 100;
+
+        public decimal CalTotalAmount(IDictionary<string, int> buyItem)
+        {
+            IDictionary<string, int> itemCnt = new Dictionary<string, int>();
+            decimal totalAmt = 0;
+
+            int x = 100;
+
+
+            foreach (var item in buyItem)
+            {
+                if (item.Value > 0)
+                {
+                    if (itemCnt.ContainsKey(item.Key))
+                    {
+                        itemCnt[item.Key] += itemCnt[item.Key];
+                    }
+                    else
+                    {
+                        itemCnt.Add(item.Key, item.Value);
+                    }
+                }
+            }
+
+            while (itemCnt.Count > 0)
+            {
+                foreach (var item in itemCnt)
+                {
+                    if (itemCnt.Count == 1)
+                    {
+                        totalAmt += _price;
+                    }
+                    else if (itemCnt.Count == 2)
+                    {
+                        totalAmt += _price * (decimal)0.95;
+                    }
+                    else if (itemCnt.Count == 3)
+                    {
+                        totalAmt += _price * (decimal)0.9;
+                    }
+                    else if (itemCnt.Count == 4)
+                    {
+                        totalAmt += _price * (decimal)0.8;
+                    }
+                    else if (itemCnt.Count == 5)
+                    {
+                        totalAmt += _price * (decimal)0.75;
+                    }
+                    else
+                    {
+                        totalAmt += _price;
+                    }
+                }
+
+                foreach (var key in itemCnt.Keys.ToList())
+                {
+                    itemCnt[key] -= 1;
+
+                    if (itemCnt[key] == 0)
+                    {
+                        itemCnt.Remove(key);
+                    }
+                }
+            }
+
+
+            return totalAmt;
+        }
+    }
+}
---
+++ PotterShoppingCart.BLLTests/ShoppingCartTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PotterShoppingCart.BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PotterShoppingCart.BLL.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void Test_購買一本價格應為100元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = 100 * 1;
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+        [TestMethod()]
+        public void Test_購買不同二本價格應為190元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = (decimal)(100 * 2 * 0.95);
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+            buyItem.Add("PotterBook-2", 1);
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+        [TestMethod()]
+        public void Test_購買不同三本價格應為270元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = (decimal)(100 * 3 * 0.9);
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+            buyItem.Add("PotterBook-2", 1);
+            buyItem.Add("PotterBook-3", 1);
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+
+        [TestMethod()]
+        public void Test_購買不同四本價格應為320元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = (decimal)(100 * 4 * 0.8);
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+            buyItem.Add("PotterBook-2", 1);
+            buyItem.Add("PotterBook-3", 1);
+            buyItem.Add("PotterBook-4", 1);
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+
+        [TestMethod()]
+        public void Test_購買不同五本價格應為375元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = (decimal)(100 * 5 * 0.75);
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+            buyItem.Add("PotterBook-2", 1);
+            buyItem.Add("PotterBook-3", 1);
+            buyItem.Add("PotterBook-4", 1);
+            buyItem.Add("PotterBook-5", 1);
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+
+        [TestMethod()]
+        public void Test_購買一二集各一本_第三集二本價格應為370元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = (decimal)(100 * 3 * 0.9 + 100);
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+            buyItem.Add("PotterBook-2", 1);
+            buyItem.Add("PotterBook-3", 2);
+            buyItem.Add("PotterBook-4", 0);
+            buyItem.Add("PotterBook-5", 0);
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+        [TestMethod()]
+        public void Test_購買一集一本_第二三集各二本價格應為460元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = (decimal)(100 * 3 * 0.9 + 100 * 2 * 0.95);
+            IDictionary<string, int> buyItem = new Dictionary<string, int>();
+            buyItem.Add("PotterBook-1", 1);
+            buyItem.Add("PotterBook-2", 2);
+            buyItem.Add("PotterBook-3", 2);
+            buyItem.Add("PotterBook-4", 0);
+            buyItem.Add("PotterBook-5", 0);
+            var actual = target.CalTotalAmount(buyItem);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+    
+    }
+}
